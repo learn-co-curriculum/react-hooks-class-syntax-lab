1cfd12d17b33da09b2710178bc6e74400df6eb80
Mohammed Shaikh <46161085+mqshaikh8@users.noreply.github.com>
2021-09-14 15:39:23 -0400

    commit tree 9825e7b82d2e3bf5a656afe1fc295e2425000ee7

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
@@ -7,14 +7,16 @@ import About from "./About";
 import user from "../data/user";
 console.log(user);
 
-function App() {
-  return (
-    <div>
-      <NavBar />
-      <Home username={user.name} city={user.city} color={user.color} />
-      <About bio={user.bio} links={user.links} />
-    </div>
-  );
+class App extends Component{
+  render(){
+    return(
+      <div>
+        <NavBar />
+        <Home username={user.name} city={user.city} color={user.color} />
+        <About bio={user.bio} links={user.links} />
+      </div>
+    )
+  }
 }
 
 export default App;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,14 @@
-import React from "react";
+import React,{Component} from "react";
 
-function Home(props) {
-  return (
-    <div id="home">
-      <h1 style={{ color: props.color }}>
-        {props.username} is a Web Developer from {props.city}
-      </h1>
-    </div>
-  );
+class Home extends Component{
+  render(){
+    return (
+      <div id="home">
+        <h1 style={{ color: this.props.color }}>
+          {this.props.username} is a Web Developer from {this.props.city}
+        </h1>
+      </div>
+    );
+  }
 }
-
 export default Home;
